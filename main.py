f108b532e6ee60c212040f9e34044b559fff9978
Hylke <hylkest2002@gmail.com>
2024-04-24 18:04:31 +0200

    Fixes

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import random
 
-
 class Player:
-    def __init__(self, leven=100, level=1):
+    def __init__(self, role, leven=100, level=1):
         self.leven = leven
         self.level = level
+        self.role = role
 
     def Attack(self):
         damage = random.randint(1, 10)
         self.leven -= damage
-        self.level += random.randint(1,5)
-        print(f"Nieuw leven:{self.leven} Nieuw level {self.level}")
+        self.level += random.randint(1, 5)
+        print(f"Je hebt iemand aangevallen. Jouw leven is nu: {self.leven}% !")
 
+    def info(self):
+        print(f"Leven: {self.leven} Level: {self.level} Rol: {self.role}")
 
-speler1 = Player()
+speler1 = Player("test")
 
 speler1.Attack()
+speler1.info()
